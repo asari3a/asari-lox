b6517b3ae5688632d768386343eecae86d6fd9d9
Ryuya Asada <ryuya0v0mac@gmail.com>
2025-10-05 20:52:07 +0900

    テストを修正

--- run.sh
+++ run.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-./asari-lox "() {}"
+assert() {
+    input=$1
+    expected=$2
+
+    actual=$(./asari-lox "$input")
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $expected"
+    else
+        echo "$input => $actual"
+        exit 1
+    fi
+}
+
+assert "" "12"
+assert "() {}" "0 1 2 3 12"
+assert "() {} !=" "0 1 2 3 11 12"
+assert "() {} !" "0 1 2 3 10 12"
